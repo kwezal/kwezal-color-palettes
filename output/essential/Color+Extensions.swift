40add93540b9e8911b6e2f7214ebd4955abfa12c
kaj <hi@kajetansuchanski.pl>
2026-06-01 18:55:03 +0200

    Initial commit

---
+++ output/essential/Color+Extensions.swift
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2026 Kwezal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import struct SwiftUI.Color
+
+extension Color {
+    static let emerald1 = Color(red: 222.0 / 255.0, green: 255.0 / 255.0, blue: 122.0 / 255.0)
+    static let emerald2 = Color(red: 196.0 / 255.0, green: 255.0 / 255.0, blue: 20.0 / 255.0)
+    static let emerald3 = Color(red: 137.0 / 255.0, green: 235.0 / 255.0, blue: 0.0 / 255.0)
+    static let emerald4 = Color(red: 85.0 / 255.0, green: 204.0 / 255.0, blue: 0.0 / 255.0)
+    static let emerald5 = Color(red: 0.0 / 255.0, green: 173.0 / 255.0, blue: 14.0 / 255.0)
+    static let emerald6 = Color(red: 0.0 / 255.0, green: 133.0 / 255.0, blue: 11.0 / 255.0)
+    static let emerald7 = Color(red: 0.0 / 255.0, green: 92.0 / 255.0, blue: 15.0 / 255.0)
+    static let turquoise1 = Color(red: 173.0 / 255.0, green: 255.0 / 255.0, blue: 221.0 / 255.0)
+    static let turquoise2 = Color(red: 92.0 / 255.0, green: 255.0 / 255.0, blue: 201.0 / 255.0)
+    static let turquoise3 = Color(red: 0.0 / 255.0, green: 235.0 / 255.0, blue: 176.0 / 255.0)
+    static let turquoise4 = Color(red: 0.0 / 255.0, green: 204.0 / 255.0, blue: 187.0 / 255.0)
+    static let turquoise5 = Color(red: 0.0 / 255.0, green: 163.0 / 255.0, blue: 163.0 / 255.0)
+    static let turquoise6 = Color(red: 0.0 / 255.0, green: 112.0 / 255.0, blue: 122.0 / 255.0)
+    static let turquoise7 = Color(red: 0.0 / 255.0, green: 77.0 / 255.0, blue: 92.0 / 255.0)
+    static let sapphire1 = Color(red: 184.0 / 255.0, green: 243.0 / 255.0, blue: 255.0 / 255.0)
+    static let sapphire2 = Color(red: 112.0 / 255.0, green: 231.0 / 255.0, blue: 255.0 / 255.0)
+    static let sapphire3 = Color(red: 71.0 / 255.0, green: 194.0 / 255.0, blue: 255.0 / 255.0)
+    static let sapphire4 = Color(red: 41.0 / 255.0, green: 148.0 / 255.0, blue: 255.0 / 255.0)
+    static let sapphire5 = Color(red: 0.0 / 255.0, green: 106.0 / 255.0, blue: 255.0 / 255.0)
+    static let sapphire6 = Color(red: 0.0 / 255.0, green: 68.0 / 255.0, blue: 204.0 / 255.0)
+    static let sapphire7 = Color(red: 0.0 / 255.0, green: 48.0 / 255.0, blue: 143.0 / 255.0)
+    static let amethyst1 = Color(red: 245.0 / 255.0, green: 214.0 / 255.0, blue: 255.0 / 255.0)
+    static let amethyst2 = Color(red: 235.0 / 255.0, green: 173.0 / 255.0, blue: 255.0 / 255.0)
+    static let amethyst3 = Color(red: 214.0 / 255.0, green: 133.0 / 255.0, blue: 255.0 / 255.0)
+    static let amethyst4 = Color(red: 187.0 / 255.0, green: 92.0 / 255.0, blue: 255.0 / 255.0)
+    static let amethyst5 = Color(red: 148.0 / 255.0, green: 41.0 / 255.0, blue: 255.0 / 255.0)
+    static let amethyst6 = Color(red: 102.0 / 255.0, green: 0.0 / 255.0, blue: 245.0 / 255.0)
+    static let amethyst7 = Color(red: 72.0 / 255.0, green: 0.0 / 255.0, blue: 173.0 / 255.0)
+    static let spinel1 = Color(red: 255.0 / 255.0, green: 204.0 / 255.0, blue: 234.0 / 255.0)
+    static let spinel2 = Color(red: 255.0 / 255.0, green: 173.0 / 255.0, blue: 221.0 / 255.0)
+    static let spinel3 = Color(red: 255.0 / 255.0, green: 133.0 / 255.0, blue: 214.0 / 255.0)
+    static let spinel4 = Color(red: 255.0 / 255.0, green: 92.0 / 255.0, blue: 201.0 / 255.0)
+    static let spinel5 = Color(red: 255.0 / 255.0, green: 41.0 / 255.0, blue: 184.0 / 255.0)
+    static let spinel6 = Color(red: 204.0 / 255.0, green: 0.0 / 255.0, blue: 136.0 / 255.0)
+    static let spinel7 = Color(red: 143.0 / 255.0, green: 0.0 / 255.0, blue: 95.0 / 255.0)
+    static let ruby1 = Color(red: 255.0 / 255.0, green: 204.0 / 255.0, blue: 213.0 / 255.0)
+    static let ruby2 = Color(red: 255.0 / 255.0, green: 153.0 / 255.0, blue: 170.0 / 255.0)
+    static let ruby3 = Color(red: 255.0 / 255.0, green: 112.0 / 255.0, blue: 136.0 / 255.0)
+    static let ruby4 = Color(red: 255.0 / 255.0, green: 71.0 / 255.0, blue: 102.0 / 255.0)
+    static let ruby5 = Color(red: 255.0 / 255.0, green: 20.0 / 255.0, blue: 79.0 / 255.0)
+    static let ruby6 = Color(red: 204.0 / 255.0, green: 0.0 / 255.0, blue: 68.0 / 255.0)
+    static let ruby7 = Color(red: 133.0 / 255.0, green: 0.0 / 255.0, blue: 44.0 / 255.0)
+    static let amber1 = Color(red: 255.0 / 255.0, green: 219.0 / 255.0, blue: 184.0 / 255.0)
+    static let amber2 = Color(red: 255.0 / 255.0, green: 184.0 / 255.0, blue: 112.0 / 255.0)
+    static let amber3 = Color(red: 255.0 / 255.0, green: 158.0 / 255.0, blue: 61.0 / 255.0)
+    static let amber4 = Color(red: 255.0 / 255.0, green: 124.0 / 255.0, blue: 31.0 / 255.0)
+    static let amber5 = Color(red: 255.0 / 255.0, green: 85.0 / 255.0, blue: 0.0 / 255.0)
+    static let amber6 = Color(red: 184.0 / 255.0, green: 61.0 / 255.0, blue: 0.0 / 255.0)
+    static let amber7 = Color(red: 122.0 / 255.0, green: 41.0 / 255.0, blue: 0.0 / 255.0)
+    static let gold1 = Color(red: 255.0 / 255.0, green: 246.0 / 255.0, blue: 143.0 / 255.0)
+    static let gold2 = Color(red: 255.0 / 255.0, green: 238.0 / 255.0, blue: 51.0 / 255.0)
+    static let gold3 = Color(red: 255.0 / 255.0, green: 219.0 / 255.0, blue: 41.0 / 255.0)
+    static let gold4 = Color(red: 255.0 / 255.0, green: 191.0 / 255.0, blue: 0.0 / 255.0)
+    static let gold5 = Color(red: 235.0 / 255.0, green: 156.0 / 255.0, blue: 0.0 / 255.0)
+    static let gold6 = Color(red: 143.0 / 255.0, green: 83.0 / 255.0, blue: 0.0 / 255.0)
+    static let gold7 = Color(red: 92.0 / 255.0, green: 54.0 / 255.0, blue: 0.0 / 255.0)
+    static let silver1 = Color(red: 249.0 / 255.0, green: 250.0 / 255.0, blue: 251.0 / 255.0)
+    static let silver2 = Color(red: 223.0 / 255.0, green: 228.0 / 255.0, blue: 236.0 / 255.0)
+    static let silver3 = Color(red: 191.0 / 255.0, green: 202.0 / 255.0, blue: 217.0 / 255.0)
+    static let silver4 = Color(red: 151.0 / 255.0, green: 165.0 / 255.0, blue: 186.0 / 255.0)
+    static let silver5 = Color(red: 108.0 / 255.0, green: 124.0 / 255.0, blue: 147.0 / 255.0)
+    static let silver6 = Color(red: 73.0 / 255.0, green: 89.0 / 255.0, blue: 110.0 / 255.0)
+    static let silver7 = Color(red: 38.0 / 255.0, green: 49.0 / 255.0, blue: 64.0 / 255.0)
+    static let onyx = Color(red: 0.0 / 255.0, green: 0.0 / 255.0, blue: 0.0 / 255.0)
+}
